dd1440cfe8b1e602fc3de2db24e221a78c80d177
MartinHoffmann85 <138533657+MartinHoffmann85@users.noreply.github.com>
2023-07-24 05:26:34 +0200

    Back Button Added and logic

--- script.js
+++ script.js
@@ -1,65 +1,69 @@
 function init() {
   fetchAndDisplayPokemon();
 }
-  
-  // Function to fetch Pokemon data from the API and display cards with images
-  async function fetchAndDisplayPokemon() {
-    const apiUrl = 'https://api.pokemontcg.io/v2/cards';
-  
-    try {
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      const cardsContainer = document.querySelector('.content');
-  
-      // Get the first 10 Pokemon cards from the API response
-      const pokemonData = data.data.slice(0, 10);
-  
-      // Create and append card elements with background images to the container
-      pokemonData.forEach((pokemon) => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.style.backgroundImage = `url(${pokemon.images.small})`;
-      
-        // Add event listener to handle the click event
-        card.addEventListener('click', () => {
-          handleCardClick(card);
-        });
-      
-        cardsContainer.appendChild(card);
-      });
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
+
+// Function to fetch Pokemon data from the API and display cards with images
+async function fetchAndDisplayPokemon() {
+  const apiUrl = "https://api.pokemontcg.io/v2/cards";
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    const cardsContainer = document.querySelector(".content");
+
+    // Get the first 10 Pokemon cards from the API response
+    const pokemonData = data.data.slice(0, 10);
+
+    // Create and append card elements with background images to the container
+    displayPokemonImage(pokemonData, cardsContainer);
+  } catch (error) {
+    console.error("Error fetching data:", error);
   }
+}
 
-  function handleCardClick(card) {
-    // Create a new div container for the enlarged card and stats button
-    const enlargedCardContainer = document.createElement('div');
-    enlargedCardContainer.classList.add('enlarged-card-container');
-    enlargedCardContainer.classList.add('displayFlex');
-  
-    // Clone the clicked card and add it to the new container
-    const enlargedCard = card.cloneNode(true);
-    enlargedCard.classList.add('enlarged-card');
-  
-    // Remove the animation class from the cloned card
-    enlargedCard.classList.remove('active');
-  
-    // Add a new class to the cloned card to disable the animation
-    enlargedCard.classList.add('no-animation');
-  
-    enlargedCardContainer.appendChild(enlargedCard);
-  
-    // Create the "Stats" button and append it to the container
-    const statsButton = document.createElement('button');
-    statsButton.innerText = 'Stats';
-    statsButton.classList.add('stats-button');
-    enlargedCardContainer.appendChild(statsButton);
+function displayPokemonImage(pokemonData, cardsContainer) {
+  pokemonData.forEach((pokemon) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.style.backgroundImage = `url(${pokemon.images.small})`;
 
-    // Add event listener to the "Stats" button to call openStats() when clicked
+    // Add event listener to handle the click event
+    card.addEventListener("click", () => {
+      handleCardClick(card);
+    });
+
+    cardsContainer.appendChild(card);
+  });
+}
+
+function handleCardClick(card) {
+  // Create a new div container for the enlarged card and stats button
+  const enlargedCardContainer = document.createElement("div");
+  enlargedCardContainer.classList.add("enlarged-card-container");
+  enlargedCardContainer.classList.add("displayFlex");
+
+  // Clone the clicked card and add it to the new container
+  const enlargedCard = card.cloneNode(true);
+  enlargedCard.classList.add("enlarged-card");
+
+  // Remove the animation class from the cloned card
+  enlargedCard.classList.remove("active");
+
+  // Add a new class to the cloned card to disable the animation
+  enlargedCard.classList.add("no-animation");
+
+  enlargedCardContainer.appendChild(enlargedCard);
+
+  // Create the "Stats" button and append it to the container
+  const statsButton = document.createElement("button");
+  statsButton.innerText = "Stats";
+  statsButton.classList.add("stats-button");
+  enlargedCardContainer.appendChild(statsButton);
+
+  // Add event listener to the "Stats" button to call openStats() when clicked
   let statsButtonClicked = false; // Variable to keep track of whether the "Stats" button was clicked
 
-  statsButton.addEventListener('click', () => {
+  statsButton.addEventListener("click", () => {
     if (!statsButtonClicked) {
       statsButtonClicked = true;
       openStats();
@@ -70,41 +74,61 @@ function init() {
       }, 500);
     }
   });
-  
-    // Position the "Stats" button below the enlarged card
-    const cardRect = card.getBoundingClientRect();
-    const statsButtonTop = cardRect.bottom + 20; // Add some spacing (20px) below the card    
-    statsButton.style.left = '50%';
-    statsButton.style.transform = 'translateX(-50%)';
-    statsButton.style.top = `${statsButtonTop}px`;
-  
-    // Append the new container to the body
-    document.querySelector('body').appendChild(enlargedCardContainer);
-  
-    // Add event listener to remove the enlarged card and container when clicking outside
-    enlargedCardContainer.addEventListener('click', (event) => {
-      if (!enlargedCard.contains(event.target)) {
-        enlargedCardContainer.remove();
-      }
-    });
-  }
 
-  function openStats() {
-    // Find the content container
-    const contentContainer = document.querySelector('.content');
-    contentContainer.classList.add('vh100');
-    // Remove all existing content (cards)
-    contentContainer.innerHTML = '';
-  
-    // Create a new card element with a blue background
-    const newCard = document.createElement('div');
-    newCard.classList.add('card');    
-    newCard.style.backgroundColor = 'blue';
+  // Position the "Stats" button below the enlarged card
+  const cardRect = card.getBoundingClientRect();
+  const statsButtonTop = cardRect.bottom + 20; // Add some spacing (20px) below the card
+  statsButton.style.left = "50%";
+  statsButton.style.transform = "translateX(-50%)";
+  statsButton.style.top = `${statsButtonTop}px`;
 
-    // Remove the animation classes from the new card
-    newCard.classList.remove('active');
-    newCard.style.animation = 'none';
-  
-    // Append the new card to the content container
-    contentContainer.appendChild(newCard);      
-  }
+  // Append the new container to the body
+  document.querySelector("body").appendChild(enlargedCardContainer);
+
+  // Add event listener to remove the enlarged card and container when clicking outside
+  enlargedCardContainer.addEventListener("click", (event) => {
+    if (!enlargedCard.contains(event.target)) {
+      enlargedCardContainer.remove();
+    }
+  });
+}
+
+function openStats() {
+  // Find the content container
+  const contentContainer = document.querySelector(".content");
+  contentContainer.classList.add("vh100");
+  // Remove all existing content (cards)
+  contentContainer.innerHTML = "";
+
+  // Create a new card element with a blue background
+  const newCard = document.createElement("div");
+  newCard.classList.add("card");
+  newCard.style.backgroundColor = "white";
+
+  // Remove the animation classes from the new card
+  newCard.classList.remove("active");
+  newCard.style.animation = "none";
+
+  // Append the new card to the content container
+  contentContainer.appendChild(newCard);
+
+  // Create the "back" button and append it to the container
+  const backButton = document.createElement("button");
+  backButton.innerText = "Back";
+  backButton.classList.add("back-button"); // Use a different class name for the "Back" button
+  contentContainer.appendChild(backButton);
+
+  // Add event listener to the "Back" button to call backButton() when clicked
+  backButton.addEventListener("click", backButtonHandler);
+}
+
+function backButtonHandler() {
+  // Find the content container
+  const contentContainer = document.querySelector(".content");
+
+  // Remove all existing content (cards)
+  contentContainer.innerHTML = "";
+
+  // Fetch and display Pokemon images again
+  fetchAndDisplayPokemon();
+}
